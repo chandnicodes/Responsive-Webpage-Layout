5f0d2463f9679102f3117929fe4d797b7981ca0b
Chandni <kumari.chandni9710@gmail.com>
2023-03-22 20:21:28 +0530

    initial commit all files

---
+++ script.js
@@ -0,0 +1,31 @@
+// fixed header background color change on scrollY ----
+const navbar = document.querySelector('.main-header');
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('fixed-header');
+    } else {
+        navbar.classList.remove('fixed-header');
+    }
+};
+// navlist active items 
+let navLinks = document.getElementsByClassName("nav-link");
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    let currentItem = document.getElementsByClassName("active");
+    currentItem[0].className = currentItem[0].className.replace(" active", "");
+    this.className += " active";
+  });
+};
+// toggle menu on small screens
+function toglemenu() {
+    let togglemenu = document.querySelector(".navbar-collapse");
+    if (togglemenu.style.display === "block") {
+        togglemenu.style.display = "none";
+    } else {
+        togglemenu.style.display = "block";
+    }
+};
+// owl carousel 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
